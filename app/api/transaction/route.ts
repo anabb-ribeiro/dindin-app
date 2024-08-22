de23feaf13f9c9f3fcf7bfa951ecd63b6e31a4bb
Ana Beatriz Barreto <abbmr@hotmail.com>
2024-08-21 23:28:55 -0300

    :sparkles: feat post and get for transaction

--- app/api/transaction/route.ts
+++ app/api/transaction/route.ts
@@ -1,6 +1,7 @@
 import { prisma } from "../../../prisma/client"
 import { NextRequest, NextResponse } from "next/server";
 import { transactionBodySchema, categorySchema } from "../zodSchema";
+import { auth } from "../auth/[...nextauth]/route"
 
 interface transactionBody {
   userId: string;
@@ -11,6 +12,15 @@ interface transactionBody {
 }
 
 export async function POST(request: NextRequest) {
+  const session = await auth()
+  console.log(session)
+  if (!session
+    || session.user == undefined
+    || session.user.id == undefined
+    || typeof session.user == "string") {
+    return NextResponse.json("Internal Error sem session.user", { status: 500 })
+  }
+
   const body: transactionBody = await request.json();
   const bodyValidation = transactionBodySchema.safeParse(body)
   const categoryValidation = categorySchema.safeParse(body.category);
@@ -24,7 +34,7 @@ export async function POST(request: NextRequest) {
   try {
     const newTransaction = await prisma.transaction.create({
       data: {
-        userId: body.userId,
+        userId: session.user.id,
         category: categoryValidation.data,
         value: body.value,
         date: body.date ? body.date : new Date(),
@@ -37,6 +47,19 @@ export async function POST(request: NextRequest) {
   }
 }
 
-export async function GET(request: NextRequest) {
+export async function GET(req: NextRequest, res: NextResponse) {
+  const session = await auth()
+  if (!session || session.user == undefined) {
+    return NextResponse.json("Internal Error sem session.user", { status: 500 })
+  }
+
+  try {
+    const transactions = await prisma.transaction.findMany({
+      where: { userId: session.user.id }
+    })
+    return NextResponse.json(transactions, { status: 200 })
+  } catch (error) {
+    return NextResponse.json("Internal Error erro no prisma", { status: 500 })
+  }
 
 }
