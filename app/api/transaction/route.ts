748573b8884d3f960f7720fd190d391c908ed008
Ana Beatriz Barreto <abbmr@hotmail.com>
2024-08-22 20:11:06 -0300

    :recycle: refactor: created verifySession to use in all routes that need userId

--- app/api/transaction/route.ts
+++ app/api/transaction/route.ts
@@ -1,6 +1,6 @@
 import { prisma } from "../../../prisma/client"
 import { NextRequest, NextResponse } from "next/server";
-import { transactionBodySchema, categorySchema } from "../zodSchema";
+import { transactionBodySchema, categorySchema, transactionUpdateBodySchema } from "../zodSchema";
 import { auth } from "../auth/[...nextauth]/route"
 
 interface transactionBody {
@@ -11,19 +11,27 @@ interface transactionBody {
   date?: string;
 }
 
-export async function POST(request: NextRequest) {
+export async function verifySession() {
   const session = await auth()
-  console.log(session)
-  if (!session
-    || session.user == undefined
-    || session.user.id == undefined
-    || typeof session.user == "string") {
-    return NextResponse.json("Internal Error sem session.user", { status: 500 })
-  }
 
+  if (!session
+    || !session.user
+    || !session.user.id
+    || typeof session.user.id !== "string") {
+    return null;
+  }
+  return session
+}
+
+export async function POST(request: NextRequest) {
+  const session = await verifySession()
   const body: transactionBody = await request.json();
   const bodyValidation = transactionBodySchema.safeParse(body)
   const categoryValidation = categorySchema.safeParse(body.category);
+
+  if (!session) {
+    return NextResponse.json("Internal Error sem session.user", { status: 500 })
+  }
 
   if (!bodyValidation.success) {
     return NextResponse.json(bodyValidation.error, { status: 400 })
@@ -34,7 +42,7 @@ export async function POST(request: NextRequest) {
   try {
     const newTransaction = await prisma.transaction.create({
       data: {
-        userId: session.user.id,
+        userId: session.user!.id!,
         category: categoryValidation.data,
         value: body.value,
         date: body.date ? body.date : new Date(),
@@ -48,14 +56,15 @@ export async function POST(request: NextRequest) {
 }
 
 export async function GET(req: NextRequest, res: NextResponse) {
-  const session = await auth()
-  if (!session || session.user == undefined) {
+  const session = await verifySession()
+
+  if (!session) {
     return NextResponse.json("Internal Error sem session.user", { status: 500 })
   }
 
   try {
     const transactions = await prisma.transaction.findMany({
-      where: { userId: session.user.id }
+      where: { userId: session.user!.id }
     })
     return NextResponse.json(transactions, { status: 200 })
   } catch (error) {
@@ -63,3 +72,4 @@ export async function GET(req: NextRequest, res: NextResponse) {
   }
 
 }
+
